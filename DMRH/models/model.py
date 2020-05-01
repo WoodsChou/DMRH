26d7382fe6c125c6cf41b8653b98490afeeebc91
WoodsChou <64581299+WoodsChou@users.noreply.github.com>
2020-05-01 16:38:10 +0800

    Create model.py

---
+++ DMRH/models/model.py
@@ -0,0 +1,76 @@
+# model.py
+# coding = utf-8
+
+import torch
+import torch.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self, out_features, pretrain_model=None):
+        super(Model, self).__init__()
+        self.features = nn.Sequential(
+            # 0 conv1
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=11, stride=4),
+            # 1 relu1
+            nn.ReLU(inplace=True),
+            # 2 norm1
+            nn.LocalResponseNorm(size=2, k=2),
+            # 3 pool1
+            nn.ZeroPad2d((0, 1, 0, 1)),
+            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+            # 4 conv2
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=5, stride=1, padding=2),
+            # 5 relu2
+            nn.ReLU(inplace=True),
+            # 6 norm2
+            nn.LocalResponseNorm(size=2, k=2),
+            # 7 pool2
+            nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+            # 8 conv3
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            # 9 relu3
+            nn.ReLU(inplace=True),
+            # 10 conv4
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            # 11 relu4
+            nn.ReLU(inplace=True),
+            # 12 conv5
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            # 13 relu5
+            nn.ReLU(inplace=True),
+            # 14 pool5
+            nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), padding=(0, 0)),
+            # 15 full_conv6
+            nn.Conv2d(in_channels=256, out_channels=4096, kernel_size=6),
+            # 16 relu6
+            nn.ReLU(inplace=True),
+            # 17 full_conv7
+            nn.Conv2d(in_channels=4096, out_channels=4096, kernel_size=1),
+            # 18 relu7
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+        )
+        # fc8
+        self.classifier = nn.Sequential(
+            nn.Conv2d(in_channels=4096, out_channels=out_features, kernel_size=1),
+        )
+        self.classifier.children().__next__().weight.data = torch.randn(out_features, 4096, 1, 1) * 0.01
+        self.classifier.children().__next__().bias.data = torch.randn(out_features) * 0.01
+        if pretrain_model:
+            self._init(pretrain_model)
+
+    def _init(self, data):
+        weights = data['layers'][0]
+        for k, v in self.features.named_children():
+            k = int(k)
+            if isinstance(v, nn.Conv2d):
+                if k > 1:
+                    k -= 1
+                v.weight.data = torch.from_numpy(weights[k][0][0][0][0][0].transpose())
+                v.bias.data = torch.from_numpy(weights[k][0][0][0][0][1].reshape(-1))
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        x = x.reshape(x.shape[0], x.shape[1], -1)
+        return x
